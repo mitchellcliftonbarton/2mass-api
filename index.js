32a6d88747d6eab08791ea3ed3b3f6546cd2377d
Mitchell Barton <mitchell@ringseven.com>
2017-07-10 18:07:40 -0600

    debug

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ const corsOptions = {
 /**** Enables pre-flight requests ****/
 
 app.options('*', cors());
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 /**** Database Info ****/
 
